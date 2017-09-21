caf936dadf03d4726b96e6b2ea1e8b4acd657f4b
linusyoung <linusyoungrice@gmail.com>
2017-09-21 22:01:48 +1000

    part2 week6 codes all

--- Part2-Wk6.R
+++ Part2-Wk6.R
@@ -42,3 +42,47 @@ plot(ms_mod, which=4, cook.levels=cutoff)
 lmBeta(ms_mod) 
 round(pCorr(ms_mod), 4) 
 
+# Part 2 Week 6
+# Lab
+res <- TempskiResilience
+res_bsp <- res[res$Group == 'Basic Sciences',]
+cor(res_bsp$MS.QoL, res_bsp$WHOQOL.PH)
+cor(res_bsp$MS.QoL, res_bsp$WHOQOL.PSY)
+cor(res_bsp$MS.QoL, res_bsp$WHOQOL.SOC)
+cor(res_bsp$MS.QoL, res_bsp$WHOQOL.ENV)
+model <- lm(MS.QoL ~ WHOQOL.PH + WHOQOL.PSY 
+            + WHOQOL.SOC + WHOQOL.ENV, data = res_bsp)
+summary(model)
+
+lmBeta(model)
+pCorr(model)
+
+
+# Problem set
+# Question 1
+res_cli <- res[res$Group == 'Clinical Sciences',]
+cli_model <- lm(BDI ~ Age + Female + State.Anxiety + Trait.anxiety, data = res_cli)
+summary(cli_model)
+lmBeta(cli_model)
+pCorr(cli_model)
+
+# Question 2
+li_model <- lm(BDI ~ Age, data = res_cli)
+summary(li_model)
+10.92641/2.98874
+
+(1848.76/69.22)*18
+# F
+-23.4325/12.74
+# G
+0.1528 * 8.32
+# H
+(8.32^2)/((8.32^2)+18)
+
+# Question 3
+qf(0.95, 4, 88)
+# men
+3526.4 + 722.5*1 + 90.02*12 + 1.269*10 + 23.406*15
+# women
+3526.4 + 722.5*0 + 90.02*12 + 1.269*10 + 23.406*15
+
